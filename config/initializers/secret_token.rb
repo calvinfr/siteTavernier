e091ce5b26668787fdd27b5dd7fbab69f7d915d9
calvinfr <geoffroylarroumet@gmail.com>
2013-07-28 15:33:52 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteTavernier::Application.config.secret_token = '96ff722d3ee14a792d85a477b34f9de74aeeede668fbfcf3ef83a5f1599a8f9080acb3a1babb580fb50cb1e17e6edf458c46748f04cac5ea9c40957a1b25ad5d'
